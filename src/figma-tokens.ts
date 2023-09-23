b77f1a483611c58dcf5b09635f981e678183ad6c
0.engineering <kiet@0.engineering>
2023-09-23 19:36:56 -0400

    Synched src/figma-tokens.ts from 0.engineering plugin

--- src/figma-tokens.ts
+++ src/figma-tokens.ts
@@ -1,53 +1,54 @@
-export class Spacing {
-    static Standard = class {
-        public static readonly SPACING_XL = 32;
-        public static readonly SPACING_LG = 24;
-        public static readonly SPACING_SM = 12;
-        public static readonly SPACING_MD = 16;
-        public static readonly SPACING_XS = 8;
-    }
-    static Expanded = class {
-        public static readonly SPACING_XL = 64;
-        public static readonly SPACING_LG = 48;
-        public static readonly SPACING_SM = 24;
-        public static readonly SPACING_MD = 32;
-        public static readonly SPACING_XS = 16;
-    }
-    static Condensed = class {
-        public static readonly SPACING_XL = 16;
-        public static readonly SPACING_LG = 12;
-        public static readonly SPACING_SM = 4;
-        public static readonly SPACING_MD = 8;
-        public static readonly SPACING_XS = 2;
-    }
+:root {
+    --spacing-spacing-xl: 32px;
+    --spacing-spacing-lg: 24px;
+    --spacing-spacing-sm: 12px;
+    --spacing-spacing-md: 16px;
+    --spacing-spacing-xs: 8px;
+    --themes-color-dot: #00000029;
+    --themes-color-input-background: #FFFFFF;
+    --themes-color-text-primary: #000000E5;
+    --themes-color-border-primary: #0000001F;
+    --themes-color-background: #FAFAFA;
+    --themes-color-border-secondary: #00000014;
+    --themes-border-radius-medium: 4px;
+    --themes-color-text-tertiary: #0000004D;
+    --themes-border-radius-small: 2px;
+    --themes-color-button-fill: #000000D9;
+    --themes-color-button-text: #FFFFFF;
+    --themes-color-text-secondary: #00000099;
 }
-export class Themes {
-    static Minimal = class {
-        public static readonly COLOR_DOT = '#00000029';
-        public static readonly COLOR_INPUT_BACKGROUND = '#FFFFFF';
-        public static readonly COLOR_TEXT_PRIMARY = '#000000E5';
-        public static readonly COLOR_BORDER_PRIMARY = '#0000001F';
-        public static readonly COLOR_BACKGROUND = '#FAFAFA';
-        public static readonly COLOR_BORDER_SECONDARY = '#00000014';
-        public static readonly BORDER_RADIUS_MEDIUM = 4;
-        public static readonly COLOR_TEXT_TERTIARY = '#0000004D';
-        public static readonly BORDER_RADIUS_SMALL = 2;
-        public static readonly COLOR_BUTTON_FILL = '#000000D9';
-        public static readonly COLOR_BUTTON_TEXT = '#FFFFFF';
-        public static readonly COLOR_TEXT_SECONDARY = '#00000099';
-    }
-    static Rounded = class {
-        public static readonly COLOR_DOT = '#00000029';
-        public static readonly COLOR_INPUT_BACKGROUND = '#FAFAFA';
-        public static readonly COLOR_TEXT_PRIMARY = '#000000E5';
-        public static readonly COLOR_BORDER_PRIMARY = '#0000001F';
-        public static readonly COLOR_BACKGROUND = '#FFFFFF';
-        public static readonly COLOR_BORDER_SECONDARY = '#00000014';
-        public static readonly BORDER_RADIUS_MEDIUM = 12;
-        public static readonly COLOR_TEXT_TERTIARY = '#0000004D';
-        public static readonly BORDER_RADIUS_SMALL = 8;
-        public static readonly COLOR_BUTTON_FILL = '#007AFF';
-        public static readonly COLOR_BUTTON_TEXT = '#FFFFFF';
-        public static readonly COLOR_TEXT_SECONDARY = '#00000099';
-    }
+[data-spacing-mode="standard"] {
+    --spacing-spacing-xl: 32px;
+    --spacing-spacing-lg: 24px;
+    --spacing-spacing-sm: 12px;
+    --spacing-spacing-md: 16px;
+    --spacing-spacing-xs: 8px;
+}
+[data-spacing-mode="expanded"] {
+    --spacing-spacing-xl: 64px;
+    --spacing-spacing-lg: 48px;
+    --spacing-spacing-sm: 24px;
+    --spacing-spacing-md: 32px;
+    --spacing-spacing-xs: 16px;
+}
+[data-spacing-mode="condensed"] {
+    --spacing-spacing-xl: 16px;
+    --spacing-spacing-lg: 12px;
+    --spacing-spacing-sm: 4px;
+    --spacing-spacing-md: 8px;
+    --spacing-spacing-xs: 2px;
+}
+[data-themes-mode="minimal"] {
+    --themes-color-input-background: #FFFFFF;
+    --themes-color-background: #FAFAFA;
+    --themes-border-radius-medium: 4px;
+    --themes-border-radius-small: 2px;
+    --themes-color-button-fill: #000000D9;
+}
+[data-themes-mode="rounded"] {
+    --themes-color-input-background: #FAFAFA;
+    --themes-color-background: #FFFFFF;
+    --themes-border-radius-medium: 12px;
+    --themes-border-radius-small: 8px;
+    --themes-color-button-fill: #007AFF;
 }
