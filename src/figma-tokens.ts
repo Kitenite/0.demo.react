2a53b7257c7bc94ecbfb54e722c663ecd5288966
0.engineering <kiet@0.engineering>
2023-10-01 12:13:41 -0400

    Synched src/figma-tokens.ts from 0.engineering plugin

---
+++ src/figma-tokens.ts
@@ -0,0 +1,34 @@
+export class Core {
+    static Light = class {
+        public static readonly BACKGROUND_PRIMARY_ALT = '#FDFDFD';
+        public static readonly BUTTON_TEXT_SECONDARY = '#FFFFFF';
+        public static readonly BUTTON_BACKGROUND_SECONDARY = '#000000E5';
+        public static readonly BORDER_PRIMARY = '#0000001F';
+        public static readonly BORDER_RADIUS_LG = 6;
+        public static readonly BACKGROUND_TERTIARY = '#0000000A';
+        public static readonly BORDER_RADIUS_MD = 4;
+        public static readonly BORDER_RADIUS_SM = 2;
+        public static readonly BACKGROUND_SECONDARY = '#00000014';
+        public static readonly BORDER_SECONDARY = '#0000000F';
+        public static readonly TEXT_TERTIARY = '#0000004D';
+        public static readonly TEXT_SECONDARY = '#00000099';
+        public static readonly TEXT_PRIMARY = '#000000E5';
+        public static readonly BACKGROUND_PRIMARY = '#FFFFFF';
+    }
+    static Dark = class {
+        public static readonly BACKGROUND_PRIMARY_ALT = '#1B1B1B';
+        public static readonly BUTTON_TEXT_SECONDARY = '#000000E5';
+        public static readonly BUTTON_BACKGROUND_SECONDARY = '#FFFFFF';
+        public static readonly BORDER_PRIMARY = '#FFFFFF1F';
+        public static readonly BORDER_RADIUS_LG = 6;
+        public static readonly BACKGROUND_TERTIARY = '#FFFFFF0A';
+        public static readonly BORDER_RADIUS_MD = 4;
+        public static readonly BORDER_RADIUS_SM = 2;
+        public static readonly BACKGROUND_SECONDARY = '#FFFFFF14';
+        public static readonly BORDER_SECONDARY = '#FFFFFF0F';
+        public static readonly TEXT_TERTIARY = '#FFFFFF4D';
+        public static readonly TEXT_SECONDARY = '#FFFFFF99';
+        public static readonly TEXT_PRIMARY = '#FFFFFFE5';
+        public static readonly BACKGROUND_PRIMARY = '#2C2C2C';
+    }
+}
