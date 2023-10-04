6a5b940fa23d69385bbc58153f9c16c7542810d5
0.engineering <kiet@0.engineering>
2023-10-03 22:00:59 -0400

    Synched variables.ts from 0.engineering plugin

---
+++ variables.ts
@@ -0,0 +1,154 @@
+export class Tokens {
+    Mode1 = {
+        Color: {
+            ButtonLabelPrimary: "#000000",
+            ColorViolet: "#C7B9FF",
+            ButtonBackgroundPrimary: "#C7B9FF",
+            ColorBlack: "#000000",
+            ButtonOutlinePrimary: "#000000",
+        },
+        Number: {
+            RadiusSm: 8,
+            SpacingLg: 24,
+            SpacingSm: 12,
+        },
+    };
+}
+
+export class ColorsExample {
+    Light = {
+        ColorBorderPrimary: "#000000",
+        ColorSurface: "#FFFFFF",
+        ColorAccent: "#F769C7",
+        ColorTextPrimary: "#000000",
+        ColorSurfaceFaint: "#FAFAFA",
+        ColorBrand: "#3CFF8A",
+        ColorFeature: "#FF8577",
+        Input: {
+            Selected: "#FF8577",
+            ForegroundColor: "#000000",
+            BorderColor: "#000000",
+            BackgroundColor: "#FFFFFF",
+        },
+        Button: {
+            ForegroundPrimary: "#000000",
+            BorderPrimary: "#000000",
+            BackgroundPrimary: "#3CFF8A",
+        },
+    };
+    Dark = {
+        ColorBorderPrimary: "#757575",
+        ColorSurface: "#000000",
+        ColorAccent: "#699BF7",
+        ColorTextPrimary: "#FFFFFF",
+        ColorSurfaceFaint: "#2A2A2A",
+        ColorBrand: "#8061FF",
+        ColorFeature: "#FF8577",
+        Input: {
+            Selected: "#FF8577",
+            ForegroundColor: "#FFFFFF",
+            BorderColor: "#757575",
+            BackgroundColor: "#000000",
+        },
+        Button: {
+            ForegroundPrimary: "#FFFFFF",
+            BorderPrimary: "#8061FF",
+            BackgroundPrimary: "#8061FF",
+        },
+    };
+}
+
+export class TextExample {
+    English = {
+        Itemadded: true,
+        ProductPears: "Pears",
+        ProductCherriesPrice: "$2.89/lb",
+        GrownIn: "Grown in",
+        ProductPearsPrice: "$2.29/lb",
+        ProductCherries: "Cherries",
+        ProductWatermelonPrice: "$5.99/ea",
+        ProductWatermelon: "Watermelon",
+        AddToCart: "Add to cart",
+        FindAnItem: "Find an item",
+        Search: "Search",
+        HelloFriend: "Hello friend",
+    };
+    German = {
+        Itemadded: false,
+        ProductPears: "Birne",
+        ProductCherriesPrice: "2,49 €",
+        GrownIn: "Angebaut in",
+        ProductPearsPrice: "1,89 €",
+        ProductCherries: "Kirschen",
+        ProductWatermelonPrice: "3,69 €",
+        ProductWatermelon: "Wassermelone",
+        AddToCart: "In den Warenkorb",
+        FindAnItem: "Einen Gegenstand finden",
+        Search: "Suche",
+        HelloFriend: "Hallo Freund",
+    };
+    Japanese = {
+        Itemadded: false,
+        ProductPears: "洋ナシ",
+        ProductCherriesPrice: "¥ 10000",
+        GrownIn: "で育った",
+        ProductPearsPrice: "¥ 299",
+        ProductCherries: "さくらんぼ",
+        ProductWatermelonPrice: "¥ 980",
+        ProductWatermelon: "スイカ",
+        AddToCart: "カートに入れる",
+        FindAnItem: "アイテムを見つける",
+        Search: "検索",
+        HelloFriend: "こんにちは、友人",
+    };
+}
+
+export class SpacingExample {
+    Standard = {
+        SpacingXl: 32,
+        SpacingLg: 24,
+        SpacingSm: 12,
+        SpacingMd: 16,
+        SpacingXs: 8,
+    };
+    Expanded = {
+        SpacingXl: 64,
+        SpacingLg: 48,
+        SpacingSm: 24,
+        SpacingMd: 32,
+        SpacingXs: 16,
+    };
+    Condensed = {
+        SpacingXl: 16,
+        SpacingLg: 12,
+        SpacingSm: 4,
+        SpacingMd: 8,
+        SpacingXs: 2,
+    };
+}
+
+export class Themes {
+    Light = {
+        ColorBorderPrimary: "#000000",
+        ColorSurface: "#FFFFFF",
+        ColorTextPrimary: "#000000",
+        ColorBrand: "#C7B9FF",
+        Button: {
+            ForegroundPrimary: "#000000",
+            BorderPrimary: "#000000",
+            BackgroundPrimary: "#C7B9FF",
+        },
+    };
+    Dark = {
+        ColorBorderPrimary: "#757575",
+        ColorSurface: "#000000",
+        ColorTextPrimary: "#FFFFFF",
+        ColorBrand: "#8061FF",
+        Button: {
+            ForegroundPrimary: "#FFFFFF",
+            BorderPrimary: "#757575",
+            BackgroundPrimary: "#8061FF",
+        },
+    };
+}
+
